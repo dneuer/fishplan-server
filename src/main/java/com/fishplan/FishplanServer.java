f573bf02a4ffb7437796ffc23228a768fb26fa00
dneuer <dave.neuer@pobox.com>
2013-11-13 23:40:36 -0500

    Initial commit with main abstractions

---
+++ src/main/java/com/fishplan/FishplanServer.java
@@ -0,0 +1,15 @@
+package com.fishplan;
+
+import javax.ws.rs.core.Application;
+
+/**
+ * Hello world!
+ *
+ */
+public class FishplanServer extends Application
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/fishplan/Plan.java
@@ -0,0 +1,20 @@
+package com.fishplan;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.HeaderParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+
+/**
+ * Resource class for plans.
+ */
+public class Plan 
+{
+}
